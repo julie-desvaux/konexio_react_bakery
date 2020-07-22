f19b589cfaad718790a60bac2545c9f27968fed2
hoodoo2310 <desvaux.julie@gmail.com>
2020-07-22 18:04:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Add from './components/Add';
+import List from './components/List';
+import Pay from './components/Pay'
+import Button from './components/core/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      activeTabe: 'add',
+      items: [],
+    }
+  }
+
+  onClickTabAdd() {
+    console.log('onClickTabAdd');
+    this.setState({ activeTabe: 'add'})
+    
+  }
+
+  onClickTabList() {
+    console.log('onClickTabList')
+    this.setState({ activeTabe: 'list'})
+  }
+
+  onClickTabPay() {
+    console.log('onClickTabPay')
+    this.setState({ activeTabe: 'pay'})
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <div className="col">
+          <h1>Bakery</h1>
+          <Button 
+            className={"btn btn-primary"} onClick={this.onClickTabAdd}> 
+            Add
+          </Button>
+          <Button 
+            className={"btn btn-outline-secondary"} onClick={this.onClickTabList}>
+            List
+          </Button>
+          <Button 
+            className={"btn btn-outline-secondary"} onClick={this.onClickTabPay}>
+            Pay
+          </Button>
+          <Add />
+          <List />
+          <Pay />
+        </div>        
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Pay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Pay extends React.Component {
+    render() {
+        return(
+            <div>Pay</div>
+        );
+    }
+}
+
+export default Pay;
